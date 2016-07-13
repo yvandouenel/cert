e4f90a8ea086cc068ba2639942e4f27ca2a8c5d2
Yvan Douënel <y.douenel@coopernet.fr>
2016-07-13 18:43:29 +0200

    formation_open_close.js up to manage open-close print.

--- sites/all/modules/custom/feature_formation_cert/js/formation_open_close.js
+++ sites/all/modules/custom/feature_formation_cert/js/formation_open_close.js
@@ -1,14 +1,17 @@
 jQuery(function($) {
-  $('.group-title-description').each(function(){
-      $scope = $(this);
-      $('.field-name-field-description', $scope).hide();
-      $('.field-name-field-titre', $scope).addClass('description-hidden');
-      (function($scope){
-          $('.field-name-field-titre', $scope).click(function(){
-              $('.field-name-field-description', $scope).slideToggle();
-              $('.field-name-field-titre', $scope).toggleClass('description-hidden');
-              $('.field-name-field-titre', $scope).toggleClass('description-show');
-          });
-      })($scope);
-  });
+    if($('.h3-formation').length){
+        $('.group-title-description').each(function(){
+            $scope = $(this);
+            $('.field-name-field-description', $scope).hide();
+            $('.field-name-field-titre', $scope).addClass('description-hidden');
+            (function($scope){
+                $('.field-name-field-titre', $scope).click(function(){
+                    $('.field-name-field-description', $scope).slideToggle();
+                    $('.field-name-field-titre', $scope).toggleClass('description-hidden');
+                    $('.field-name-field-titre', $scope).toggleClass('description-show');
+                });
+            })($scope);
+        });
+    }
+
 });
